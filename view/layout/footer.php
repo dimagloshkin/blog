fcd64f583bbda13f09e8480dadd23f7e60f2e9b6
dimagloshkin <dimagloshkin@gmail.com>
2019-04-15 22:08:12 +0300

    added view folder with layout header, footer, sidebar

---
+++ view/layout/footer.php
@@ -0,0 +1,18 @@
+<footer id="footer">
+    <div class="container">
+        <div class="footer__logo">
+            <h1>Блог IT_Минималиста</h1>
+        </div>
+        <nav class="footer__menu">
+            <ul>
+                <li><a href="#">Главная</a></li>
+                <li><a href="#">Обо мне</a></li>
+                <li><a href="#">Я Вконтакте</a></li>
+                <li><a href="#">Правообладателям</a></li>
+            </ul>
+        </nav>
+    </div>
+</footer>
+</div>
+</body>
+</html>
---
+++ view/layout/header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Блог IT_Минималиста!</title>
+
+    <!-- Bootstrap Grid -->
+    <link rel="stylesheet" type="text/css" href="/template/media/assets/bootstrap-grid-only/css/grid12.css">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+
+    <!-- Custom -->
+    <link rel="stylesheet" type="text/css" href="/template/media/css/style.css">
+</head>
+
+<body>
+
+<div id="wrapper">
+
+    <header id="header">
+        <div class="header__top">
+            <div class="container">
+                <div class="header__top__logo">
+                    <h1>Блог IT_Минималиста</h1>
+                </div>
+                <nav class="header__top__menu">
+                    <ul>
+                        <li><a href="#">Главная</a></li>
+                        <li><a href="#">Обо мне</a></li>
+                        <li><a href="#">Я Вконтакте</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+        <div class="header__bottom">
+            <div class="container">
+                <nav>
+                    <ul>
+                        <li><a href="#">Безопасность</a></li>
+                        <li><a href="#">Программирование</a></li>
+                        <li><a href="#">Lifestyle</a></li>
+                        <li><a href="#">Музыка</a></li>
+                        <li><a href="#">Саморазвитие</a></li>
+                        <li><a href="#">Гайды</a></li>
+                        <li><a href="#">Обзоры</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+
